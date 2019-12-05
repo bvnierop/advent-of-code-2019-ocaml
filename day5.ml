d245e42a2cd0f89a3e12bcf151b567f4f2e87793
Bart van Nierop <mail@bvnierop.nl>
2019-12-05 20:43:07 +0100

    Add day5, part one

--- day5.ml
+++ day5.ml
@@ -2,9 +2,8 @@ open Util
 open IntCodeComputer
 
 let solve_a memory =
-  let _ = program_execute ~verbose: false ~print_outputs: true
-      (program_make ~inputs: [1] memory) in
-  1 |> ignore
+  program_execute ~verbose: false ~print_outputs: true
+      (program_make ~inputs: [1] memory)
     
 let _ =
   let memory = read_memory () in
