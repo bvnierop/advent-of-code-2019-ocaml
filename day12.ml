82aa3170a33abae8db780180a3a46984d0442aa8
Bart van Nierop <mail@bvnierop.nl>
2019-12-12 16:36:42 +0100

    Refactor Day 12 to modules

--- day12.ml
+++ day12.ml
@@ -1,7 +1,7 @@
 open Util
 
-module Vector = struct
-  type t = { x: int; y: int; z: int } [@@deriving show]
+module ThreeD = struct
+  type t = { x: int; y: int; z: int } 
   let empty = { x = 0; y = 0; z = 0; }
 
   let add a b =
@@ -26,6 +26,18 @@ module Vector = struct
       z = gravity_of_one a.z b.z  }
 end
 
+module OneD = struct
+  type t = int
+  let add a b = a + b
+  let read () = failwith "Not Implemented: OneD#read"
+  let absolute_sum x = x
+  let empty = 0
+  let gravity a b = 
+    if a < b then 1
+    else if a = b then 0
+    else (-1)
+end
+
 module type MOON_POSITION = sig
   type t
   val add: (t -> t -> t)
@@ -36,7 +48,7 @@ module type MOON_POSITION = sig
 end
 
 module Moon = struct
-  module  Make(MP: MOON_POSITION) = struct
+  module Make(MP: MOON_POSITION) = struct
     type t = {
       position: MP.t;
       velocity: MP.t;
@@ -63,30 +75,31 @@ module Moon = struct
 
     let gravity_of_moons a b =
       MP.gravity a.position b.position
+
+    let empty_velocity = MP.empty
+    let add_velocity a b = MP.add a b
+
+    let step moons =
+      CCList.map (fun moon ->
+          let gravity = CCList.fold_left (fun gravity other_moon -> add_velocity gravity (gravity_of_moons moon other_moon))
+              empty_velocity moons in
+          apply_gravity gravity moon) moons
+      |> CCList.map step
   end
 end
 
-module VectorMoon = Moon.Make(Vector)
-
-let step moons =
-  CCList.map (fun moon ->
-      let gravity = CCList.fold_left (fun gravity other_moon -> Vector.add gravity (VectorMoon.gravity_of_moons moon other_moon))
-          Vector.empty moons in
-      VectorMoon.apply_gravity gravity moon) moons
-  |> CCList.map VectorMoon.step
-
+module ThreeDMoon = Moon.Make(ThreeD)
+module OneDMoon = Moon.Make(OneD)
 
 let read_input () =
-  read_all VectorMoon.read
-
+  read_all ThreeDMoon.read
 
 let solve moons step_count =
-  Gen.fold (fun moons _ -> step moons) moons
+  Gen.fold (fun moons _ -> ThreeDMoon.step moons) moons
     (Gen.int_range 1 step_count)
-  |> CCList.map VectorMoon.energy
+  |> CCList.map ThreeDMoon.energy
   |> CCList.fold_left (+) 0
   |> string_of_int |> print_endline
-
 
 let _ =
   let moons = read_input () in
