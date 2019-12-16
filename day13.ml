8d12f6526dce817158ab7821111d1261e3fe15a7
Bart van Nierop <mail@bvnierop.nl>
2019-12-16 16:11:41 +0100

    Make Day 13A nicer, to prepare for part 2

--- day13.ml
+++ day13.ml
@@ -1,25 +1,88 @@
 open Util
 open IntCodeComputer
 
+module Grid = struct
+  module Tile = struct
+    type t =
+      | Empty
+      | Wall
+      | Block
+      | Paddle
+      | Ball
+
+    let of_int i =
+      match i with
+      | 0 -> Empty
+      | 1 -> Wall
+      | 2 -> Block
+      | 3 -> Paddle
+      | 4 -> Ball
+      | _ -> failwith (Printf.sprintf "Invalid tile: %d" i)
+
+    let to_string tile =
+      match tile with
+      | Empty -> " "
+      | Wall -> "#"
+      | Block -> "@"
+      | Paddle -> "_"
+      | Ball -> "o"
+  end
+  module Point = struct type t = (int * int) let compare = Stdlib.compare end
+  module PointMap = CCMap.Make(Point)
+
+  type t = Tile.t PointMap.t
+
+  let empty = PointMap.empty
+
+  let points grid = PointMap.to_list grid
+
+  let get _point _grid = Tile.Empty
+  let set point tile grid = PointMap.add point tile grid
+  let to_string _grid = ""
+end
+
+module Arcade = struct
+  type input_state =
+    | Empty
+    | X of int
+    | Y of (int * int)
+    | Tile of (int * int * int) [@@deriving show]
+
+  type t = {
+    input_state: input_state;
+    grid: Grid.t;
+  } 
+
+  let empty = { input_state = Empty; grid = Grid.empty }
+
+  let next_input_state input arcade = 
+    match arcade.input_state with
+    | Empty -> X input
+    | X x -> Y (x, input)
+    | Y (x, y) -> Tile (x, y, input)
+    | Tile (_, _, _) -> failwith "Unexpected process input state."
+
+  let update_grid x y tile arcade =
+    { arcade with grid = Grid.set (x, y) (Grid.Tile.of_int tile) arcade.grid }
+
+  let process_input input arcade =
+    let input_state = next_input_state input arcade in
+    match input_state with
+    | Empty | X _ | Y _ -> { arcade with input_state = input_state }
+    | Tile (x, y, tile) -> { (update_grid x y tile arcade) with input_state = Empty }
+end
+
 let simulate memory =
-  let output_handler = (fun output state program ->
-      match state with
+  let arcade_handler = (fun output (arcade_opt: Arcade.t option) program ->
+      match arcade_opt with
       | None -> failwith "No state given"
-      | Some (acc, count) -> let built = output :: acc in
-        if CCList.length built = 3 then begin
-          if output = 2 then
-            (Some ([], count + 1), program)
-          else
-            (Some ([], count), program)
-        end
-        else
-          (Some (built, count), program)) in
-  let program = program_with_output output_handler (program_make ~input: (Gen.empty) memory)
-      ~initial: (Some ([], 0)) in
+      | Some arcade -> ((Some (Arcade.process_input output arcade)), program)) in
+  let program = program_with_output arcade_handler (program_make ~input: (Gen.empty) memory)
+      ~initial: (Some Arcade.empty) in
   let executed = program_execute ~verbose: false ~print_outputs: false program in
   match executed.state with
   | None -> 0
-  | Some (_, count) -> count
+  | Some arcade -> Grid.points arcade.grid |> CCList.filter (fun (_, tile) -> match tile with | Grid.Tile.Block -> true | _ -> false) |> CCList.length
 
 let solve memory =
   simulate memory
